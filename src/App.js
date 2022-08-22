5874239e77904c69c4c0fb74c34876a5bb8cea8b
Fatimah Alghamdi <fatimah-ma@hotmail.com>
2022-08-22 12:36:08 +0300

    Coin Flipping

--- src/App.js
+++ src/App.js
@@ -2,22 +2,62 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  function tossCoin() {
+    return Math.random() > 0.5 ? "heads" : "tails";
+}
+
+// function fiveHeadsSync() {
+//   let headsCount = 0;
+//   let attempts = 0;
+//   while(headsCount < 5) {
+//       attempts++;
+//       let result = tossCoin();
+//       console.log(`${result} was flipped`);
+//       if(result === "heads") {
+//           headsCount++;
+//       } else {
+//           headsCount = 0;
+//       }
+//   }
+//   return `It took ${attempts} tries to flip five "heads"`;
+// }
+// console.log( fiveHeadsSync() );
+// console.log( "This is run after the fiveHeadsSync function completes" );
+
+
+  function fiveHeads() {
+    return new Promise( (resolve, reject) => {
+        // your code here!
+      let headsCount = 0;
+      let attempts = 0;
+      while(headsCount < 5) {
+        attempts++;
+        let result = tossCoin();
+        if(result === "heads") {
+          headsCount++;
+        } else {
+          headsCount = 0;
+        }
+      }
+      if (attempts < 100){
+        resolve(" Head has been flipped five times ")
+      } else {
+        reject(" Coin has been flipped more than 100 times")
+      }
+    });
+}
+
+fiveHeads()
+    .then( res => console.log(res) )
+    .catch( err => console.log(err) );
+console.log( "When does this run now?" );
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
     </div>
   );
 }
